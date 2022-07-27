58abae0623ddc2ca89ca2bf1b578760d63f5b71a
Edsson <akairu@hotmail.com>
2022-07-26 22:13:08 -0500

    advanced animations 2

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -24,6 +24,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
 
     controller.forward();
     controller.addListener(() {
+      setState(() {});
       print(controller.value);
     });
   }
@@ -31,7 +32,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.red.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
